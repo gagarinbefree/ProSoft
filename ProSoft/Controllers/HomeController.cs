680b0c52afc4ffdc9494196b75e58fcabfae9bc1
gagarinbefree <gagarinbefree@gmail.com>
2019-06-26 13:06:52 +0500

    register services

--- ProSoft/Controllers/HomeController.cs
+++ ProSoft/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using DataAccess;
 using Microsoft.AspNetCore.Mvc;
 using ProSoft.Models;
 
@@ -10,6 +11,13 @@ namespace ProSoft.Controllers
 {
     public class HomeController : Controller
     {
+        private IGenUoW _unit;
+
+        public HomeController(IGenUoW unit)
+        {
+            _unit = unit;
+        }
+
         public IActionResult Index()
         {
             return View();
--- ProSoft/Startup.cs
+++ ProSoft/Startup.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DataAccess;
+using DataAccess.Ef;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -32,6 +34,8 @@ namespace ProSoft
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddDbContext<ProSoftDbContext>();
+            services.AddScoped<IGenUoW, EfUnit>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
